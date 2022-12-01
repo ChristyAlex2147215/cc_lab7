d84954339aad593708e30c9be0626d21ae722b02
ChristyAlex2147215 <christy.alex@mca.christuniversity.in>
2022-12-01 08:10:59 +0530

    operations in db

---
+++ database.js
@@ -0,0 +1,23 @@
+const mysql = require('mysql');
+
+var connection = mysql.createConnection({
+	
+	host:"covid19.c1h8tplwfs1y.us-east-1.rds.amazonaws.com",
+	port:"3306",
+	user:"admin",
+	password:"qwertyuiop",
+	database:"Covid19"
+});
+
+connection.connect(function(error){
+	if(error)
+	{
+		throw error;
+	}
+	else
+	{
+		console.log('MySQL Database is connected Successfully');
+	}
+});
+
+module.exports = connection;
